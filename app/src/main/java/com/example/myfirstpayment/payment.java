6fd853c4e72c95ffaf584aaaf4e2f10ee417996b
Ethan Anderson <ethan.anderson188@gmail.com>
2020-01-03 01:18:17 +0000

    payment app using google pay

---
+++ app/src/main/java/com/example/myfirstpayment/payment.java
@@ -0,0 +1,127 @@
+package com.example.myfirstpayment;
+
+import android.app.Activity;
+
+import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class payment {
+    private payment(){}
+
+    //Declares the version of google pay that will be used
+    //this must be present in all other request objects
+    private static JSONObject getBaseRequest() throws JSONException {
+        return new JSONObject().put("apiVersion", 2).put("apiVersionMinor", 0);
+    }
+
+    //this choses how the payment will be processed
+    private static JSONObject getGatewayTokenizationSpecification() throws JSONException {
+        return new JSONObject(){{      put("type", "PAYMENT_GATEWAY");
+            put("parameters", new JSONObject(){{        put("gateway", "example");
+                put("gatewayMerchantId", "exampleGatewayMerchantId");
+            }
+            });
+        }};
+    }
+
+    //defines what card networks can be used in the app
+    private static JSONArray getAllowedCardNetworks() {
+        return new JSONArray()
+                .put("AMEX")
+                .put("DISCOVER")
+                .put("INTERAC")
+                .put("JCB")
+                .put("MASTERCARD")
+                .put("VISA");
+    }
+
+    //app can take cards on file at google or from authenticated devices
+    private static JSONArray getAllowedCardAuthMethods() {
+        return new JSONArray()
+                .put("PAN_ONLY")
+                .put("CRYPTOGRAM_3DS");
+    }
+
+
+
+    //describe payment methods that will be used
+    //for this app it will be card
+    private static JSONObject getBaseCardPaymentMethod() throws JSONException {
+        JSONObject cardPaymentMethod = new JSONObject();
+        cardPaymentMethod.put("type", "CARD");
+
+        JSONObject parameters = new JSONObject();
+        parameters.put("allowedAuthMethods", getAllowedCardAuthMethods());
+        parameters.put("allowedCardNetworks", getAllowedCardNetworks());
+
+        cardPaymentMethod.put("parameters", parameters);
+
+        return cardPaymentMethod;
+    }
+
+    //further describes the method of payment
+    private static JSONObject getCardPaymentMethod() throws JSONException {
+        JSONObject cardPaymentMethod = getBaseCardPaymentMethod();
+        cardPaymentMethod.put("tokenizationSpecification", getGatewayTokenizationSpecification());
+
+        return cardPaymentMethod;
+    }
+
+
+    public static Optional<JSONObject> getIsReadyToPayRequest() {
+        try {
+            JSONObject isReadyToPayRequest = getBaseRequest();
+            isReadyToPayRequest.put(
+                    "allowedPaymentMethods", new JSONArray().put(getBaseCardPaymentMethod()));
+
+            return Optional.of(isReadyToPayRequest);
+        } catch (JSONException e) {
+            return Optional.empty();
+        }
+    }
+
+
+    private static JSONObject getTransactionInfo(String price) throws JSONException {
+        JSONObject transactionInfo = new JSONObject();
+        transactionInfo.put("totalPrice", price);
+        transactionInfo.put("totalPriceStatus", "FINAL");
+        transactionInfo.put("countryCode", "GB");
+        transactionInfo.put("currencyCode", "GBP");
+
+        return transactionInfo;
+    }
+
+
+    private static JSONObject getMerchantInfo() throws JSONException {
+        return new JSONObject().put("merchantName", "Example Merchant");
+    }
+
+
+    public static Optional<JSONObject> getPaymentDataRequest(String price) {
+        try {
+            JSONObject paymentDataRequest = getBaseRequest();
+            paymentDataRequest.put("allowedPaymentMethods", new JSONArray().put(getCardPaymentMethod()));
+            paymentDataRequest.put("transactionInfo", getTransactionInfo(price));
+            paymentDataRequest.put("merchantInfo", getMerchantInfo());
+
+      /* An optional shipping address requirement is a top-level property of the PaymentDataRequest
+      JSON object. */
+            paymentDataRequest.put("shippingAddressRequired", false);
+
+            JSONArray allowedCountryCodes = new JSONArray(Arrays.asList("US", "GB"));
+
+            return Optional.of(paymentDataRequest);
+        } catch (JSONException e) {
+            return Optional.empty();
+        }
+    }
+}
